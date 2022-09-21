b8fc09393647659a4127373221a05d1386c76892
FirozSkAhmad <you@example.com>
2022-09-21 16:30:44 +0530

    upD

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -72,19 +72,61 @@ async function createBook(req, res) {
 }
 //===============================================================================================
 // _id, title, excerpt, userId, category, releasedAt, reviews field
- let fetchbooks = async function(req,res){
-    let data = req.query
-    if(Object.keys(data).length==0){return res
+let fetchbooks = async function (req, res) {
+  try {
+    let data = req.query;
+    if (Object.keys(data).length == 0) {
+      return res
         .status(400)
-        .send({ status: false, message: "required alteast one query" });}
-        data.isDeleted=false
-        let getDocs = await bookModel.find(data).select({ _id:1,title:1, excerpt:1, userId:1, category:1, releasedAt:1, reviews:1}).sort({title:1})
-         if(getDocs.length==0){
-            return res.status(404).send({ status: false, message: "No documents founded" });
-         } 
-        return res
-        .status(200)
-        .send({ status: true, msg: "success", data:getDocs});
- }
+        .send({ status: false, message: "required alteast one query" });
+    }
+    data.isDeleted = false;
+    let getDocs = await bookModel
+      .find(data)
+      .select({
+        _id: 1,
+        title: 1,
+        excerpt: 1,
+        userId: 1,
+        category: 1,
+        releasedAt: 1,
+        reviews: 1,
+      })
+      .sort({ title: 1 });
+    if (getDocs.length == 0) {
+      return res
+        .status(404)
+        .send({ status: false, message: "No documents founded" });
+    }
+    return res
+      .status(200)
+      .send({ status: true, msg: "success", data: getDocs });
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: error.message });
+  }
+};
 
-module.exports = { createBook ,fetchbooks};
+const getBooks = async function (req, res) {
+  try {
+    let id = req.params.bookId;
+    if (id === ":bookId") {
+      return res
+        .status(400)
+        .send({ status: false, message: "bookId is required" });
+    }
+    let savedData = await bookModel.findOne({ _id: id }).lean();
+    if (!savedData) {
+      return res
+        .status(404)
+        .send({ status: false, message: "No documents founded" });
+    }
+    savedData.reviewsData = [];
+    return res
+      .status(200)
+      .send({ status: true, msg: "success", data: savedData });
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: error.message });
+  }
+};
+
+module.exports = { createBook, fetchbooks, getBooks };
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
 const bookController = require("../controller/bookController");
-const authContoller=require("../auth/auth")
+const authContoller = require("../auth/auth");
 
 router.get("/test-me", function (req, res) {
   res.send("this is successfully created");
@@ -10,6 +10,16 @@ router.get("/test-me", function (req, res) {
 
 router.post("/register", userController.createUser);
 router.post("/login", userController.login);
-router.post("/books",authContoller.authentication,authContoller.authorization, bookController.createBook);
-router.get("/books",authContoller.authentication,bookController.fetchbooks)
+router.post(
+  "/books",
+  authContoller.authentication,
+  authContoller.authorization,
+  bookController.createBook
+);
+router.get("/books", authContoller.authentication, bookController.fetchbooks);
+router.get(
+  "/books/:bookId",
+  authContoller.authentication,
+  bookController.getBooks
+);
 module.exports = router;
